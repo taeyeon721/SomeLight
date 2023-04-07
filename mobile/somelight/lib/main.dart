4a00fe0d1fe0bd435d102a60d774f878821abb97
조은비 <immt12478@naver.com>
2023-04-07 11:44:05 +0900

    [MOBILE]fix: fix mobile

--- mobile/somelight/lib/main.dart
+++ mobile/somelight/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:kakao_flutter_sdk/kakao_flutter_sdk_user.dart';
-
-import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
-import 'package:somelight/mypage.dart';
-import 'package:somelight/sample_screen.dart';
+// import 'package:flutter_localizations/flutter_localizations.dart/';
+// import 'package:intl/intl.dart';
+// import 'package:flutter_boilerplate/generated/l10n.dart';
 import 'package:somelight/story.dart';
+import 'package:flutter_animator/flutter_animator.dart';
+import 'package:transition/transition.dart';
 
 void main() {
 
@@ -12,10 +12,10 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   // runApp() 호출 전 Flutter SDK 초기화
-  KakaoSdk.init(
-      nativeAppKey: 'f7176e7c4e491caaa3b9b327a213ae41',
-      javaScriptAppKey: 'aa78c7b2bc6cec332bb7b843647e900b',
-  );
+  // KakaoSdk.init(
+  //     nativeAppKey: 'f7176e7c4e491caaa3b9b327a213ae41',
+  //     javaScriptAppKey: 'aa78c7b2bc6cec332bb7b843647e900b',
+  // );
   runApp(const SomeLight());
 }
 
@@ -31,15 +31,90 @@ class SomeLight extends StatelessWidget{
       //   '/':(context) => const SomeLight(),
       //   '/mypage':(context) => const MyPage(),
       // },
-
+      // localizationsDelegates: const [
+      //   // S.delegate,
+      //   GlobalMaterialLocalizations.delegate,
+      //   GlobalWidgetsLocalizations.delegate,
+      //   GlobalCupertinoLocalizations.delegate,
+      // ],
+      // supportedLocales: const [
+      //   Locale('en', ''), //English, no country code
+      //   Locale('ko', ''),
+      // ],
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.cyan,
+        fontFamily: "Dovemayo_gothic",
       ),
-      home: const StoryCreatePage(),
+      home: Homepage()
     );
   }
 }
 
+class Homepage extends StatelessWidget{
 
+  @override
+  Widget build(BuildContext context){
+    MediaQueryData deviceData = MediaQuery.of(context);
+    Size screenSize = deviceData.size;
+    return Scaffold(
+      body: Center(
+        child:Container(
+          width: screenSize.width,
+          height: screenSize.height,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Colors.blue.shade100,
+                Colors.purple.shade100,
+                Colors.pink.shade100
+              ]
+            )
+
+          ),
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Text(
+                "SOMELIGHT",
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 65,
+                ),
+              ),
+            ),
+            Container(
+              width: screenSize.width*(0.1),
+              height: screenSize.height*(0.05),
+            ),
+            Container(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple.shade300,
+                ),
+                onPressed: ()=>{
+                  Navigator.push(
+                    context,
+                    Transition(
+                child: StoryCreatePage(),
+                transitionEffect: TransitionEffect.SCALE)
+                 )},
+                child: Text(
+                    "시작하기",
+                  style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                ),),
+                ),
+              ),
+          ],
+        ),
+      ),
+      ));
+  }
+}
 
